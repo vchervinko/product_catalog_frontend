79a8540380d80182b17a560ec3ff139e61f7481f
Ruslan Pashkov <nothingsfading@gmail.com>
2023-07-29 19:13:27 +0300

    Update provider import for phones page

--- src/pages/PhonesPage.tsx
+++ src/pages/PhonesPage.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { PhonesContextProvider } from '../context/PhonesContext/PhonesContextProvider';
+import { PhonesContextProvider } from '../contexts/PhonesContext/PhonesContextProvider';
 
 const PhonesPage: FC = () => {
   return (
