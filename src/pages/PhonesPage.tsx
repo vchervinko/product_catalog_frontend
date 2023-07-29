614400558f007604869571a9f5116839be8516af
AlexKherson <q0992358472@gmail.com>
2023-07-29 17:38:31 +0200

    refactoring rename AAA to PhonesContext

--- src/pages/PhonesPage.tsx
+++ src/pages/PhonesPage.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { PhonesContextProvider } from '../context/AAA/PhonesContextProvider';
+import { PhonesContextProvider } from '../context/PhonesContext/PhonesContextProvider';
 
 const PhonesPage: FC = () => {
   return (
