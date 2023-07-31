4cc325208cea6cb1ea8d4b1b4a68302ae4d6e9e2
Ruslan Pashkov <nothingsfading@gmail.com>
2023-07-31 15:15:02 +0300

    Add Container component to Layout.tsx

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,19 @@
 import { FC } from 'react';
 import { Outlet } from 'react-router-dom';
-import { Header } from '../Header';
+import { Container } from '../Container';
 import { Footer } from '../Footer';
+import { Header } from '../Header';
 
 export const Layout: FC = () => (
   <>
     <Header />
-    <Outlet />
-    <Footer />
+
+    <Container>
+      <Outlet />
+    </Container>
+
+    <Container>
+      <Footer />
+    </Container>
   </>
 );
