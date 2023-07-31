937a52c6e940ef367b4e9c09e55723a0acfc155b
Ruslan Pashkov <nothingsfading@gmail.com>
2023-07-31 21:15:30 +0300

    Minimize layout structure

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,19 +1,12 @@
 import { FC } from 'react';
 import { Outlet } from 'react-router-dom';
-import { Container } from '../Container';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 
 export const Layout: FC = () => (
   <>
     <Header />
-
-    <Container>
-      <Outlet />
-    </Container>
-
-    <Container>
-      <Footer />
-    </Container>
+    <Outlet />
+    <Footer />
   </>
 );
