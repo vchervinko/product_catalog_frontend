ae2d7bd8115972ae0bb372e81e494882aefe604c
Ruslan Pashkov <nothingsfading@gmail.com>
2023-07-31 20:12:12 +0300

    Add Logo component

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../../assets/icons/NiceGadgetsLogo.svg';
+import logo from '../../assets/icons/Logo.svg';
 import './Logo.scss';
 
 interface Props {
