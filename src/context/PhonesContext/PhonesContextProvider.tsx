b3d49c68afb35e02e35aaec182179c966d3537bb
AlexKherson <q0992358472@gmail.com>
2023-08-01 09:43:00 +0200

    added card layout

--- src/context/PhonesContext/PhonesContextProvider.tsx
+++ src/context/PhonesContext/PhonesContextProvider.tsx
@@ -1,16 +1,44 @@
-import { FC, ReactNode, memo } from 'react';
+import { FC, ReactNode, memo, useCallback, useMemo, useState } from 'react';
 import { Props as PhoneContextProps, PhonesContext } from './PhonesContext';
+import { Product } from '../../types/Product';
+import { findItemById } from '../../utils/collectionsHelper/findItemById';
 
 interface Props {
   children: ReactNode;
 }
 
-const value: PhoneContextProps = {
-  total: 15,
-  limit: 2,
-};
-
 export const PhonesContextProvider: FC<Props> = memo(({ children }) => {
+
+  const [cart, setCart] = useState<Product[]>([]);
+  const [likedProducts, setLikedProducts] = useState<Product[]>([]);
+
+  const addProductToCart = useCallback((product: Product) => {
+    setCart((prevCart) => [...prevCart, product]);
+  }, [cart]);
+
+  const toggleLikeProduct = useCallback((product: Product) => {
+    setLikedProducts((prevLikedProducts) => {
+      const foundedProduct = findItemById(prevLikedProducts, product.id);
+
+      if (foundedProduct) {
+        return prevLikedProducts
+          .filter(likedProduct => likedProduct.id !== product.id);
+      }
+
+      return [...prevLikedProducts, product];
+    });
+  }, [likedProducts]);
+
+  const value: PhoneContextProps = useMemo(() => ({
+    total: 15,
+    limit: 2,
+    cart,
+    addProductToCart,
+    likedProducts,
+    toggleLikeProduct,
+
+  }), [cart, likedProducts]);
+
   return (
     <PhonesContext.Provider value={value}>{children}</PhonesContext.Provider>
   );
--- src/pages/PhonesPage.tsx
+++ src/pages/PhonesPage.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 import { PhonesContextProvider } from '../context/PhonesContext/PhonesContextProvider';
 import { Pagination } from '../components/Pagination';
+import { ProductCard } from '../components/ProductCard';
 
 const PhonesPage: FC = () => (
   <PhonesContextProvider>
     <main>Phones Page</main>
+    <ProductCard product={{id: 5}}/>
     <Pagination />
   </PhonesContextProvider>
 );
